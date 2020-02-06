acd58d635aa90ee3e79b236747e32c41b6a2edbf
Tyrell Cotton <tyrell.cotton@codeimmersives.com>
2020-02-06 12:31:33 -0500

    Function update

--- rollercoaster.js
+++ rollercoaster.js
@@ -2,9 +2,14 @@ function ride(height, weight)
 num1 = Number(height)
 num2 = Number(weight)
 
-if (num1 <= 84 || (num1 >= 48 ) ){
-    return 'Eligible to ride coaster'
+if (num1 >= 84 || (num1 <= 48 ) ){
+    return 'Height ineligible to ride coaster'
 }
-else if(num2 <= 300 || (num2 >= 100)){
-    return 'Eligible to ride coaster'
+else if(num2 >= 300 || (num2 <= 100)){
+    return 'Weight ineligible to ride coaster'
 }
+else if(num1 <= 84 || (num1 >= 48 ) && (num2 <= 300 || num2 >= 100)){
+    return 'Height and Weight eligible'
+}
+else()
+    return 'Go away'
