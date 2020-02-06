326e0a904251aab007be7e706f123935c5a6b46f
Tyrell Cotton <tyrell.cotton@codeimmersives.com>
2020-02-06 14:38:19 -0500

    Updted code

---
+++ rollercoaster-front.js
@@ -0,0 +1,13 @@
+const ride = require('./rollercoaster.js')
+
+let num1 = process.argv[2]
+let num2 = process.argv[3]
+console.log(num1)
+ride(num1,num2)
+console.log(ride(num1,num2))
+
+//Input height and weight in inches
+
+
+
+
--- rollercoaster.js
+++ rollercoaster.js
@@ -1,6 +1,7 @@
-function ride(height, weight)
-num1 = Number(height)
-num2 = Number(weight)
+function ride(height, weight){
+let num1 = Number(height)
+let num2 = Number(weight)
+
 
 if (num1 >= 84 || (num1 <= 48 ) ){
     return 'Height ineligible to ride coaster'
@@ -9,5 +10,10 @@ else if(num2 >= 300 || (num2 <= 100)){
     return 'Weight ineligible to ride coaster'
 }
 
-else('') 
-    return 'Go away'
+else { 
+    return 'Eligible'
+}
+}
+
+
+module.exports = ride;
